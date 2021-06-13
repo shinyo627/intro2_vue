bdcf9eec582118cda204f8ecd2565b247afb9583
shinyo627 <shinyo627@gmail.com>
2021-06-13 12:12:50 +0200

    Created circle_classes and circle_styles computed properties for dynamic styling and binding classes for elements in document.

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 let vm = Vue.createApp({
-}).mount('#app')
+  data() {
+    return {
+      isPurple: false,
+      selectedColor: '',
+      size: 150,
+    };
+  },
+  computed: {
+    circle_classes() {
+      return { purple: this.isPurple };
+    },
+    circle_styles() {
+      return [
+        {
+          width: this.size + 'px',
+          height: this.size + 'px',
+          lineHeight: this.size + 'px',
+        },
+        { transform: 'rotate(30deg)' },
+      ];
+    },
+  },
+}).mount('#app');
